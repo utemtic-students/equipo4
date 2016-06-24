65667a74befa74c1769c412a5625def39fb7a6ec
davidmunoztrevino <a20130436@utem.edu.mx>
2016-06-24 16:17:58 -0500

    ConfigCorreos

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,11 +5,13 @@ config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-      port: 587,
+      port: 465,
       authentication: "plain",
+      domain:          "gmail.com",  
       enable_starttls_auto: true,
-      user_name: "a20130436@utem.edu.mx",
-      password: "Dmunoz94"
+      user_name: "morrallavirtual@gmail.com",
+      email: "morrallavirtual@gmail.com",
+      password: "Default123"
 
   }
 

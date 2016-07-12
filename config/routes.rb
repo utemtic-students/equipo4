9ac3329abac828d4ecbdac0da1746ba08d07b177
Alfredo Siprian <fredy15917@gmail.com>
2016-07-12 18:32:15 -0500

    se creo el index de Administracion de Impresiones y su ruta

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'Impresiones' => 'print_admin#index', as: :impAd
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

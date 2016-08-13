f998c70986c20531b71cf70366113dfcd1a163ed
davidmunoztrevino <a20130436@utem.edu.mx>
2016-08-13 18:14:00 -0500

    Modulos

---
+++ app/controllers/prints_controller.rb
@@ -0,0 +1,75 @@
+class PrintsController < ApplicationController
+	def index
+    @prints = Print.all
+  end
+
+  # GET /surveys/1
+  # GET /surveys/1.json
+  def show
+  end
+
+  # GET /surveys/new
+  def new
+    @print = Print.new
+  end
+
+  # GET /surveys/1/edit
+  def edit
+  end
+
+  # POST /surveys
+  # POST /surveys.json
+  def create
+       @print = current_user.print∫.new(print_params)
+
+    respond_to do |format|
+      if @print.save
+
+        format.html { redirect_to @print, notice: 'Print was successfully created.' }
+        format.json { render :show, status: :created, location: @print }
+      else
+        format.html { render :new }
+        format.json { render json: @print.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /surveys/1
+  # PATCH/PUT /surveys/1.json
+  def update
+    respond_to do |format|
+      if @print.update(survey_params)
+        format.html { redirect_to @print, notice: 'Survey was successfully updated.' }
+        format.json { render :show, status: :ok, location: @print }
+      else
+        format.html { render :edit }
+        format.json { render json: @print.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /surveys/1
+  # DELETE /surveys/1.json
+  def destroy
+    @print.destroy
+    respond_to do |format|
+      format.html { redirect_to prints_url, notice: 'Survey was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_survey
+      @print = Print.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def survey_params
+      if params[:print]
+        params.require(:print).permit(
+          :q40
+        )
+      end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+
   
+  get 'modulos/index'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
- 
+  resources :prints
     resources :users
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  root 'modulos#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

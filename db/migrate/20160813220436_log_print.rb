e4e61c281c861bd1e6aa2a666b660a745954290f
DarkAbsolutE <dark_necros_absolute@hotmail.com>
2016-08-13 19:42:11 -0500

    create data base

--- db/migrate/20160813220436_log_print.rb
+++ db/migrate/20160813220436_log_print.rb
@@ -1,4 +1,10 @@
 class LogPrint < ActiveRecord::Migration
   def change
+    create_table :logPrint do |t|
+      t.timestamps :dateOrder,  null: false
+      t.integer    :state,      null: false
+      t.integer    :typeOrder,  null: false
+      t.integer    :idMount,    null: false
+    end
   end
 end
--- db/migrate/20160813220450_log_morralla.rb
+++ db/migrate/20160813220450_log_morralla.rb
@@ -1,4 +1,11 @@
 class LogMorralla < ActiveRecord::Migration
   def change
+    create_table :logMorralla do |t|
+      t.integer    :idUser,     null: false
+      t.integer    :idUserAdmin,null: true
+      t.decimal    :mount,      null: false
+      t.integer    :type,       null: false
+      t.timestamps :date,       null: false
+    end
   end
 end
